e17325513afe94e3f332f1b2b7be912cbef03d12
pear <pearguacamole40zr@gmail.com>
2024-09-10 23:15:22 +0530

    add placeholder to config.py

--- config.py
+++ config.py
@@ -1,11 +1,17 @@
-RPC_ENDPOINT = 'https://eth-mainnet.g.alchemy.com/v2/BRVNfqEubNMJJfypmMCsggWcwM-JefkA'
+#Replace <<API Key here>> place holder with alchemy key
+RPC_ENDPOINT = 'https://eth-mainnet.g.alchemy.com/v2/<<API Key here>>'
+#check example below
+#RPC_ENDPOINT = 'https://eth-mainnet.g.alchemy.com/v2/BRVNfqEubNMJJfypmMCsggWcwM-JefkA'
 BEACON_DEPOSIT_CONTRACT = '0x8149745670881d99700078ede5903A1A7beBe262'
 
 
-MONGO_URI = 'mongodb+srv://kushagra:wordpass@luganode.ftepf.mongodb.net/?retryWrites=true&w=majority&appName=luganode'  # Change as needed for your MongoDB setup
+#replace <<Password Here>> placeholder with mongodb password
+MONGO_URI = 'mongodb+srv://kushagra:<<Password Here>>@luganode.ftepf.mongodb.net/?retryWrites=true&w=majority&appName=luganode'
+# See the example below here the password is wordpass
+#MONGO_URI = 'mongodb+srv://kushagra:wordpass@luganode.ftepf.mongodb.net/?retryWrites=true&w=majority&appName=luganode'
 MONGO_DB_NAME = 'eth_deposits'
 MONGO_COLLECTION_NAME = 'deposits'
 
-# Telegram settings
-TELEGRAM_TOKEN = '7267313919:AAEBjED3wTxrrJzmWljBxSdFGfjgXTwvgQM'
-TELEGRAM_CHAT_ID = '1970005451'
+# replace the placeholders with required token and chatid
+TELEGRAM_TOKEN = '<<telegram bot token>>'
+TELEGRAM_CHAT_ID = '<<telegram chatid>>'
